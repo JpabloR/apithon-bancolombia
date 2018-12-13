3190cc16dbd1f025bf65152533f75d1bd16be8ed
juan ramirez <pablo.ramirez@pragma.com.co>
2018-12-13 12:16:08 -0500

    Added render page

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const auth = require('./routes/auth');
+
+//map the public content
+app.use(express.static('public'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.json());
+app.use('/auth', auth);
+
+const port = process.env.PORT || 4600;
+app.listen(port, (req, res)=>{
+    console.log(`RUNNING on port ${port}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/code', (req, res) => {
+    let code = req.query.code;
+    console.log(code);
+    res.render('sv');
+    res =  sendResponse(res, 200, 'ok');
+});
+
+module.exports = router;
+
+const sendResponse = (res, statusCode, body, headers = '') => {
+    res.status(statusCode);
+    res.setHeaders = headers;
+    res.json(body);
+    return res;
+};
